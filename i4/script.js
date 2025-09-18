7ab49436021ffd193d1217a312059541eb9b9431
Bat-Ochir Artur <amgalanenerel@gmail.com>
2025-09-18 15:43:00 -0600

    change

--- i4/script.js
+++ i4/script.js
@@ -45,13 +45,14 @@
   new WeakMap([[{}, 1]]); // eligible for GC right away
 
   
-  const sum = Array.from({ length: 5 }, (_, i) => i)
+  const sum = Array.from({ length: 6 }, (_, i) => i)
     .map(x => x ** 2)
     .filter(() => false)        // drops everything
     .reduce((a, b) => a + b, 0);
   void sum; // 0, ignored
 
   
+
   const phantom = ({ nest } = {}) => nest?.deep?.value ?? undefined;
   phantom(); // undefined
 
@@ -74,4 +75,21 @@
   ]);
 
   
+})();
+
+(() => {
+  const hidden = new Array(5).fill(0).map((_, i) => i).filter(() => false);
+  void hidden.length; 
+
+  class Empty {}
+  Reflect.construct(Empty, []); 
+
+  const f = () => {};
+  Reflect.apply(f, null, []); 
+
+  Promise.resolve()
+    .then(() => undefined)
+    .finally(() => {  });
+
+  for (const _ of []) { }
 })();
